135a337d4e3d2ed440f5a7706efc6019968b5a80
Jonas Evangelista <jonas.eos@outlook.com>
2019-08-21 22:33:23 -0300

    chore: eslint implementation

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
+
 const routes = new Router();
 
-routes.get("/", (__request, __response) => {
-  return __response.json({
-    message: "Hello World"
-  });
-});
+routes.get('/', (__request, __response) => __response.json({
+  message: 'Hello World',
+}));
 
 export default routes;
