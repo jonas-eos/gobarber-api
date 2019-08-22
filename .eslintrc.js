12b3083fea18c2fb81693d40d371b179a712de7c
Jonas Evangelista <jonas.eos@outlook.com>
2019-08-21 22:41:59 -0300

    chore: apply eslint, prettier + editorconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,23 @@
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
   },
-  extends: ["airbnb-base"],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
   rules: {
-    "class-method-use-this": "off",
-    "no-param-reassign": "off",
-    camelcase: "off",
-    "no-unused-vars": ["erros", { argsIgnorePattern: "__next" }]
-  }
+    'prettier/prettier': 'error',
+    'class-method-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
 };
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (__request, __response) => __response.json({
-  message: 'Hello World',
-}));
+routes.get('/', (__request, __response) =>
+  __response.json({
+    message: 'Hello World',
+  })
+);
 
 export default routes;
