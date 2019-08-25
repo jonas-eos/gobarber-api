5f758270d3a30f86a580246833d04b2af4a7b871
Jonas Evangelista <jonas.eos@outlook.com>
2019-08-25 15:44:03 -0300

    chore: nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  post: 2525,
+  secure: false,
+  auth: {
+    user: 'f1642baf7cd4e2',
+    pass: '24a8ddddbf24f9',
+  },
+  default: {
+    from: 'Jonas Evangelista <jonas.eos@outlook.com>',
+  },
+};
