a6d447fad522fcd7d53ce460dc807e2fc632da2d
Jonas Evangelista <jonas.eos@outlook.com>
2019-08-25 22:24:36 -0300

    chore: show if appointment is a past and if it cancelable

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -19,7 +19,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: __request.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
