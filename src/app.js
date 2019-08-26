47614ef378f9ebed4f052bcd357091e44d840866
Jonas Evangelista <jonas.eos@outlook.com>
2019-08-25 22:51:07 -0300

    chore: error manipulation with sentry

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
+
+import sentryConfig from './config/sentry';
 import routes from './routes';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files/',
@@ -20,6 +30,18 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  /**
+   * Handle exception and show the error as a json.
+   */
+  exceptionHandler() {
+    this.server.use(async (__error, __request, __response, __next) => {
+      const errors = await new Youch(__error, __request).toJSON();
+
+      return __response.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://950b01af91834c208b27948fffd13f43@sentry.io/1541334',
+};
