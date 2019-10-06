db487fe2e973a91307f09d1e6e052c2238f0be26
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-06 16:15:11 -0300

    chore: return customer name in schedule controller

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -26,6 +26,13 @@ class ScheduleController {
         canceled_at: null,
         date: { [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)] },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
       order: ['date'],
     });
     return __response.json({ appointments });
